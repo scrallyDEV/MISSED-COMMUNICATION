88629afc260876d23c462596244504292ed420b3
sentrydown23 <ashhailey226@gmail.com>
2026-09-20 10:38:43 -0700

    Door Noises

--- source/interactables/Door.hx
+++ source/interactables/Door.hx
@@ -1,12 +1,16 @@
 package interactables;
 
+import godot.AudioStream;
+import godot.AudioStreamPlayer3D;
 import godot.StaticBody3D;
 import godot.Vector3;
 import godot.Tween;
 
+import haxegd.Nodes.*;
 import haxegd.MathExtension;
 import haxegd.Tweening;
 import haxegd.TweenStyle;
+import haxegd.Audio3D;
 
 import entities.player.Player;
 
@@ -18,16 +22,22 @@ class Door extends StaticBody3D implements Interactable
     private var openSpeed:Float = 3;
     @:meta(export)
     private var closeSpeed:Float = 2;
+    @:meta(export)
+    private var openSound:AudioStream;
+    @:meta(export)
+    private var closeSound:AudioStream;
     
     private var isOpen:Bool = false; // door always begins closed
     private var swingDegrees:Float;
     private var swingTween:Tween;
+    private var doorAudio:AudioStreamPlayer3D = new AudioStreamPlayer3D();
 
     public function onReady():Void
     {
         swingDegrees = (swingDirection == "left") ? 90 : -90;
-
         swingDegrees = MathExtension.degreesToRadians(swingDegrees);
+
+        createChild(this, doorAudio);
     }
 
     public function activate(?player:Player):Void // can be called by any script (public function, not static so unique to each door instance)
@@ -42,10 +52,12 @@ class Door extends StaticBody3D implements Interactable
 
             if (!isOpen) { // if closed
                 Tweening.doTween(this, swingTween, "rotation", new Vector3(rotation.x, rotation.y + swingDegrees, rotation.z), TweenStyle.QuartOut, openSpeed);
+                Audio3D.doOneShot(doorAudio, openSound);
                 isOpen = !isOpen; // isOpen is whatever isOpen currently is NOT (basically a switch)
             }
             else if (isOpen) { // ditto but if open
                 Tweening.doTween(this, swingTween, "rotation", new Vector3(rotation.x, rotation.y - swingDegrees, rotation.z), TweenStyle.QuadOut, closeSpeed);
+                Audio3D.doOneShot(doorAudio, closeSound);
                 isOpen = !isOpen; // isOpen is whatever isOpen currently is NOT (basically a switch)
             }
         }
