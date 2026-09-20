cc1c253f0206b043a052fab5521e001b34d0fbc5
sentrydown23 <ashhailey226@gmail.com>
2026-09-20 11:35:56 -0700

    Flashlight Charger SFX

--- source/interactables/FlashlightCharger.hx
+++ source/interactables/FlashlightCharger.hx
@@ -1,19 +1,33 @@
 package interactables;
 
-import haxegd.MathExtension;
+import godot.AudioStream;
 import godot.StaticBody3D;
+import godot.AudioStreamPlayer3D;
 
 import entities.player.Flashlight;
 import entities.player.Player;
 
+import haxegd.Audio3D;
+import haxegd.MathExtension;
+import haxegd.Nodes.*;
+
 class FlashlightCharger extends StaticBody3D implements Interactable
 {
     private var flashlightInserted:Bool = false;
+    private var audioPlayer:AudioStreamPlayer3D = new AudioStreamPlayer3D();
 
     @:meta(export)
     private var chargeInterval:Float = 1;
     @:meta(export)
     private var failureChance:Float = 0.1; // default 10% chance to fail
+    @:meta(export)
+    private var insertSound:AudioStream;
+    @:meta(export)
+    private var retrieveSound:AudioStream;
+    @:meta(export)
+    private var chargeSound:AudioStream;
+    @:meta(export)
+    private var failureSound:AudioStream;
 
 
     // Runtime Vars
@@ -25,6 +39,8 @@ class FlashlightCharger extends StaticBody3D implements Interactable
     public function onReady():Void
     {
         failureChance = MathExtension.clamp(failureChance, 0, 1); // ensuring failure chance cannot be below 0 or above 1
+        createChild(this, audioPlayer);
+        Audio3D.setPolyphony(audioPlayer, 4); // plenty of headroom
     }
 
     public function activate(?player:Player):Void
@@ -35,10 +51,12 @@ class FlashlightCharger extends StaticBody3D implements Interactable
                 initialBattery = player.flashlight.battery;
                 flashlightInserted = true;
                 player.hasFlashlight = false;
+                Audio3D.doOneShot(audioPlayer, insertSound);
             }
             else if (flashlightInserted) {
                 flashlightInserted = false;
                 player.hasFlashlight = true;
+                Audio3D.doOneShot(audioPlayer, retrieveSound);
             }
         }
         else {
@@ -66,11 +84,13 @@ class FlashlightCharger extends StaticBody3D implements Interactable
     private function chargerTick(target:Flashlight):Void
     {
         if (Math.random() <= failureChance && canFail) { 
+            Audio3D.doOneShot(audioPlayer, failureSound);
             return; 
         }
         else { 
             target.battery += 1;
             target.battery = MathExtension.clamp(target.battery, 0, 100);
+            Audio3D.doOneShot(audioPlayer, chargeSound);
             
             if (target.battery >= initialBattery + 5) { 
                 canFail = false; 
